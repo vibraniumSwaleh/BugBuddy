2d766a1a1581ae1af16c6b69a156828393be9374
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-04-15 18:35:47 +0100

    start app UI components

--- public/App.js
+++ public/App.js
@@ -1,11 +1,73 @@
 "use strict";
 
-var continents = ["Africa", "America", "Asia", "Australia", "Europe"];
-var helloContinents = Array.from(continents, function (c) {
-  return "Hello ".concat(c, "!");
-});
-var message = helloContinents.join(" ");
-var element = /*#__PURE__*/React.createElement("div", {
-  title: "Outer div"
-}, /*#__PURE__*/React.createElement("h1", null, message));
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+function _callSuper(t, o, e) { return o = _getPrototypeOf(o), _possibleConstructorReturn(t, _isNativeReflectConstruct() ? Reflect.construct(o, e || [], _getPrototypeOf(t).constructor) : o.apply(t, e)); }
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+function _isNativeReflectConstruct() { try { var t = !Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); } catch (t) {} return (_isNativeReflectConstruct = function _isNativeReflectConstruct() { return !!t; })(); }
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+var IssueFilter = /*#__PURE__*/function (_React$Component) {
+  _inherits(IssueFilter, _React$Component);
+  function IssueFilter() {
+    _classCallCheck(this, IssueFilter);
+    return _callSuper(this, IssueFilter, arguments);
+  }
+  _createClass(IssueFilter, [{
+    key: "render",
+    value: function render() {
+      return /*#__PURE__*/React.createElement("div", null, "This is a placehodler for the issue filter.");
+    }
+  }]);
+  return IssueFilter;
+}(React.Component);
+var IssueTbale = /*#__PURE__*/function (_React$Component2) {
+  _inherits(IssueTbale, _React$Component2);
+  function IssueTbale() {
+    _classCallCheck(this, IssueTbale);
+    return _callSuper(this, IssueTbale, arguments);
+  }
+  _createClass(IssueTbale, [{
+    key: "render",
+    value: function render() {
+      return /*#__PURE__*/React.createElement("div", null, "This is a placehodler for the table of issues.");
+    }
+  }]);
+  return IssueTbale;
+}(React.Component);
+var IssueAdd = /*#__PURE__*/function (_React$Component3) {
+  _inherits(IssueAdd, _React$Component3);
+  function IssueAdd() {
+    _classCallCheck(this, IssueAdd);
+    return _callSuper(this, IssueAdd, arguments);
+  }
+  _createClass(IssueAdd, [{
+    key: "render",
+    value: function render() {
+      return /*#__PURE__*/React.createElement("div", null, "This is a placehodler for a form to add an issue.");
+    }
+  }]);
+  return IssueAdd;
+}(React.Component);
+var IssueList = /*#__PURE__*/function (_React$Component4) {
+  _inherits(IssueList, _React$Component4);
+  function IssueList() {
+    _classCallCheck(this, IssueList);
+    return _callSuper(this, IssueList, arguments);
+  }
+  _createClass(IssueList, [{
+    key: "render",
+    value: function render() {
+      return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Issue Tracker"), /*#__PURE__*/React.createElement(IssueFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueTbale, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueAdd, null));
+    }
+  }]);
+  return IssueList;
+}(React.Component);
+var element = /*#__PURE__*/React.createElement(IssueList, null);
 ReactDOM.render(element, document.getElementById("content"));
--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,10 +1,33 @@
-const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
-const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-const message = helloContinents.join(" ");
+class IssueFilter extends React.Component {
+  render() {
+    return <div>This is a placehodler for the issue filter.</div>;
+  }
+}
+class IssueTbale extends React.Component {
+  render() {
+    return <div>This is a placehodler for the table of issues.</div>;
+  }
+}
+class IssueAdd extends React.Component {
+  render() {
+    return <div>This is a placehodler for a form to add an issue.</div>;
+  }
+}
 
-const element = (
-  <div title="Outer div">
-    <h1>{message}</h1>
-  </div>
-);
+class IssueList extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTbale />
+        <hr />
+        <IssueAdd />
+      </React.Fragment>
+    );
+  }
+}
+
+const element = <IssueList />;
 ReactDOM.render(element, document.getElementById("content"));
