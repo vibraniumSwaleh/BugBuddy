15e4b868713124ec3649d438582654c70df63a85
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-05-17 23:33:17 +0100

    Done setup  using MongoDB driver for CRUD to local DB

--- scripts/testmongo.js
+++ scripts/testmongo.js
@@ -1,16 +1,37 @@
-import MongoClient from "mongodb";
+import { MongoClient } from "mongodb";
 
 // Atlas URL - replace UUU with user, PPP with password, XXX with hostname
 // const url = 'mongodb+srv://UUU:PPP@cluster0-XXX.mongodb.net/issuetracker?retryWrites=true';
-const url = "mongodb://192.168.233.168/bugbuddy:27017";
+const url = "mongodb://127.0.0.1:27017";
+const dbName = "bugbuddy";
 
-const client = MongoClient(url, { useNewUrlParser: true });
-client.connect((err, client) => {
-  const db = client.db();
-  const collection = db.collection("employees");
-  const employee = { id: 1, name: "A. Callback", age: 23 };
-  
-  collection.insertOne(employee, (err, result) => {
-    console.log("Result of insert:\n", result.insertedId);
-  });
-});
+async function testWithAsync() {
+  console.log("\n--- testWithAsync ---", "\n");
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB", "\n");
+    const db = client.db(dbName);
+    console.log(`Currently using database: ${db.databaseName}`, "\n");
+
+    const collection = db.collection("employees");
+    const employee = {
+      id: 2,
+      name: { first: "Mani", last: "Garry" },
+      age: 25,
+    };
+    const result = await collection.insertOne(employee);
+    console.log("Result of insert:\n", result, "\n");
+
+    const docs = await collection.find().toArray();
+    console.log("Result of find:\n", docs, "\n");
+
+  } catch (error) {
+    console.log(error);
+  } finally {
+    client.close();
+  }
+}
+
+testWithAsync();
