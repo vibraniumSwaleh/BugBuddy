48396d1ff7638c420421c7eef1a0ef6db4792f2d
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-05-24 10:54:00 +0100

    complete api server setup with env. variables

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,12 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 
-const PORT = 8000;
+dotenv.config();
+
+const PORT = process.env.UI_SERVER_PORT || 8000;
 const app = express();
 const pageServer = express.static("public");
 
 app.use("/", pageServer);
 
-app.listen(PORT, () =>
-{
+app.listen(PORT, () => {
   console.log(`UI server listening on port ${PORT}`);
 });
